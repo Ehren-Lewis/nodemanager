92e3820e7d72a33fcf9f2f2c776fcef20228b83e
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2025-07-01 22:10:20 -0500

    committing to transfer devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { FrameCorners } from '@arwes/react-frames'
 
-export default function Home() {
+const Sandbox = (): React.JSX.Element => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    // The parent element of the frame component should be positioned.
+    <div
+      style={{
+        position: 'relative',
+        display: 'inline-block',
+        margin: '1rem',
+        padding: '1rem 2rem'
+      }}
+    >
+      {/* Any frame component by default will take all space inside the nearest
+          positioned parent. You can change this behaviour by using frame property
+          `positioned = false`. */}
+      <FrameCorners
+        style={{
+          // @ts-expect-error css variables
+          '--arwes-frames-bg-color': 'hsl(180, 75%, 10%)',
+          '--arwes-frames-line-color': 'hsl(180, 75%, 50%)'
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          // The frame elements properties can be changed using CSS like:
+          // ```css
+          // [data-frame=bg] {
+          //   color: hsl(180, 75%, 10%);
+          // }
+          // [data-frame=line] {
+          //   color: hsl(180, 75%, 50%);
+          // }
+          // ```
+        }}
+      />
+
+      {/* The frame component is positioned so the other elements which need
+          to be on top should also be positioned. You can change this behaviour
+          by using z-index property of any of them. */}
+      <div style={{ position: 'relative', color: 'hsl(180, 75%, 50%)' }}>
+        Futuristic Sci-Fi UI Web Framework
+      </div>
     </div>
-  );
+  )
 }
+
+export default Sandbox;
