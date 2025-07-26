2d46f58fc8350faca2f86034fae3d1e343e4dfa2
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2025-07-26 02:39:30 -0500

    commiting to disable eslint errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
     TODOISTAPI: process.env.TODOISTAPI,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+module.exports = nextConfig;
