52e363c945464300b1e5c8182ac8f243ddc8eff2
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2025-07-26 02:50:47 -0500

    fixed styling issue with arwes and typescript

--- src/components/tempModularCard/tempCard.tsx
+++ src/components/tempModularCard/tempCard.tsx
@@ -2,17 +2,16 @@ import { FrameNefrex } from '@arwes/react-frames';
 import { GridLines, Dots, MovingLines } from "@arwes/react-bgs";
 import { Animator } from "@arwes/react-animator";
 
-
+const tempStyle = {
+  '--arwes-frames-bg-color': 'hsla(286, 38.50%, 43.30%, 0.46)',
+  '--arwes-frames-line-color': 'hsl(315, 98.20%, 55.30%)',
+} as React.CSSProperties;
 
 export const CardItem = ({ title }: { title: string }) => (
   
+  
   <div className="relative w-72 h-20 my-4 mx-2">
-    <FrameNefrex
-      style={{
-        '--arwes-frames-bg-color': 'hsla(286, 38.50%, 43.30%, 0.46)',
-        '--arwes-frames-line-color': 'hsl(315, 98.20%, 55.30%)',
-      }}
-    />
+    <FrameNefrex style={tempStyle} />
     <div className="absolute inset-0 flex items-center justify-center text-lg font-orbitron z-10 text-center">
       {title}
     </div>
--- src/components/today/todayCard.tsx
+++ src/components/today/todayCard.tsx
@@ -1,14 +1,18 @@
 import { FrameLines, FrameNefrex } from "@arwes/react-frames";
 
-const TodayCard = ({ title }: { title: string }) => (
-  <div className="relative w-72 h-20 my-4 mx-2">
-    <FrameLines
-      style={{
+
+const todayStyle = {
         '--arwes-frames-bg-color': 'hsla(286, 38.50%, 43.30%, 0.46)',
         '--arwes-frames-line-color': 'hsl(315, 98.20%, 55.30%)',
         "--arwes-frames-deco-color": "hsl(286, 77.50%, 27.80%)",
       // "hsl(61, 75.20%, 30.00%)
-      }}
+      } as React.CSSProperties;
+
+
+const TodayCard = ({ title }: { title: string }) => (
+  <div className="relative w-72 h-20 my-4 mx-2">
+    <FrameLines
+      style={todayStyle}
     />
     <div className="absolute inset-0 flex items-center text-center justify-center text-lg font-orbitron z-10">
       {title}
